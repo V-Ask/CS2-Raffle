944bd8c169244cd150db3f426ba9cb7ecdd786d8
Victor Ask Justesen <vict2292@gmail.com>
2024-05-09 17:07:29 +0200

    added electron

---
+++ frontend/main.js
@@ -0,0 +1,13 @@
+import {app, BrowserWindow} from 'electron'
+
+let mainWindow;
+app.whenReady().then(() => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadURL('http://localhost:3000');
+});
