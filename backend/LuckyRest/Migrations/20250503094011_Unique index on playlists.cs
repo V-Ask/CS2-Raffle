9a4442536efa29f02e37994ff2b71a07a55d9439
Victor Ask Justesen <2234-au704346@users.noreply.gitlab.au.dk>
2025-05-29 14:00:36 +0200

    Implement Backend - Added database migrations

---
+++ backend/LuckyRest/Migrations/20250503094011_Unique index on playlists.cs
@@ -0,0 +1,182 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace LuckyRest.Migrations
+{
+    /// <inheritdoc />
+    public partial class Uniqueindexonplaylists : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Playlists_AspNetUsers_AuthorId",
+                table: "Playlists");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkshopMapWorkshopPlaylist_Playlists_PlaylistsCollectionNa~",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WorkshopMapWorkshopPlaylist",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkshopMapWorkshopPlaylist_PlaylistsCollectionName",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Playlists",
+                table: "Playlists");
+
+            migrationBuilder.DropColumn(
+                name: "PlaylistsCollectionName",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlaylistsWorkshopPlaylistId",
+                table: "WorkshopMapWorkshopPlaylist",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Playlists",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "WorkshopPlaylistId",
+                table: "Playlists",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WorkshopMapWorkshopPlaylist",
+                table: "WorkshopMapWorkshopPlaylist",
+                columns: new[] { "MapsWorkshopMapId", "PlaylistsWorkshopPlaylistId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Playlists",
+                table: "Playlists",
+                column: "WorkshopPlaylistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkshopMapWorkshopPlaylist_PlaylistsWorkshopPlaylistId",
+                table: "WorkshopMapWorkshopPlaylist",
+                column: "PlaylistsWorkshopPlaylistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Playlists_CollectionName_AuthorId",
+                table: "Playlists",
+                columns: new[] { "CollectionName", "AuthorId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Playlists_AspNetUsers_AuthorId",
+                table: "Playlists",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkshopMapWorkshopPlaylist_Playlists_PlaylistsWorkshopPlay~",
+                table: "WorkshopMapWorkshopPlaylist",
+                column: "PlaylistsWorkshopPlaylistId",
+                principalTable: "Playlists",
+                principalColumn: "WorkshopPlaylistId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Playlists_AspNetUsers_AuthorId",
+                table: "Playlists");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkshopMapWorkshopPlaylist_Playlists_PlaylistsWorkshopPlay~",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_WorkshopMapWorkshopPlaylist",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkshopMapWorkshopPlaylist_PlaylistsWorkshopPlaylistId",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Playlists",
+                table: "Playlists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Playlists_CollectionName_AuthorId",
+                table: "Playlists");
+
+            migrationBuilder.DropColumn(
+                name: "PlaylistsWorkshopPlaylistId",
+                table: "WorkshopMapWorkshopPlaylist");
+
+            migrationBuilder.DropColumn(
+                name: "WorkshopPlaylistId",
+                table: "Playlists");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlaylistsCollectionName",
+                table: "WorkshopMapWorkshopPlaylist",
+                type: "character varying(50)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Playlists",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_WorkshopMapWorkshopPlaylist",
+                table: "WorkshopMapWorkshopPlaylist",
+                columns: new[] { "MapsWorkshopMapId", "PlaylistsCollectionName" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Playlists",
+                table: "Playlists",
+                column: "CollectionName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkshopMapWorkshopPlaylist_PlaylistsCollectionName",
+                table: "WorkshopMapWorkshopPlaylist",
+                column: "PlaylistsCollectionName");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Playlists_AspNetUsers_AuthorId",
+                table: "Playlists",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkshopMapWorkshopPlaylist_Playlists_PlaylistsCollectionNa~",
+                table: "WorkshopMapWorkshopPlaylist",
+                column: "PlaylistsCollectionName",
+                principalTable: "Playlists",
+                principalColumn: "CollectionName",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
